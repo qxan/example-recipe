02a940297baa2183c3eb1a29d7883a7a0c99fe6e
qxan <mehmet.enfa@gmail.com>
2022-10-27 14:28:19 +0300

    tools

---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='genediv'>
+        <div className='baslik'>
+            <h1>ABOUT THIS PAGE</h1>
+        </div>
+        <div className='tools'>
+            <p>React</p>
+            <p>HTML</p>
+            <p>TAILWIND CSS</p>
+            <p>JAVASCRIPT</p>
+            <p>AXIOS</p>
+            <p>REACT ROUTER DOM v6</p>
+            <p>REACT ICONS</p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default About
