ad6283c48fab56358e61f03f4241271d3a9423b6
qxan <mehmet.enfa@gmail.com>
2022-10-27 14:30:09 +0300

    links

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,12 @@ const About = () => {
             <p>REACT ROUTER DOM v6</p>
             <p>REACT ICONS</p>
         </div>
-        
+        <div className='mylinks'>
+            <h1>Social Media Links</h1>
+            <a href="https://www.linkedin.com/in/menfavakitsayan/">Linkedin</a>
+            <a href="https://github.com/qxan">Github</a>
+            <a href="https://github.com/qxan/react-reacipe-app">Source Code</a>
+        </div>
     </div>
   )
 }
