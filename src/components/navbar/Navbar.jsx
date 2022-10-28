14dcfb00ea42f1952a3ac7b80670d0f7ece50aba
qxan <mehmet.enfa@gmail.com>
2022-10-28 11:42:17 +0300

    about ok

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='bg-stone-300 flex justify-between h-[6rem]'>
-        <div className='ml-5 p-8 w-[50rem] flex justify-center items-center text-2xl text-cyan-700'>
-            <h1>Delicous</h1>
+    <div className="bg-stone-300 flex justify-between h-[6rem]">
+      <div className="ml-5 p-8 w-[50rem] flex justify-center items-center text-2xl text-cyan-700">
+        <h1>Delicous</h1>
+      </div>
+      <div className="p-8 w-[60rem] flex justify-center items-center text-xl text-white">
+        <Link to="/home" className="text-red-700">
+          Home
+        </Link>
+        <div className="flex gap-12 ">
+          <NavLink className="hover:text-red-700" to="/About" >About</NavLink>
+          <a className="hover:text-red-700">Github</a>
+          <NavLink className="hover:text-red-700" to="/" >Logout</NavLink>
         </div>
-        <div className='p-8 w-[60rem] flex justify-center items-center text-xl text-white'>
-                <Link to="/home" className='text-red-700'>Home</Link>
-            <div className='flex gap-12 '>
-                <NavLink className='hover:text-red-700'>About</NavLink>
-                <a className='hover:text-red-700'>Github</a>
-                <NavLink className='hover:text-red-700'>Logout</NavLink>
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,13 +4,17 @@ import coding from '../assets/coding.svg'
 const About = () => {
   return (
     <div className='genediv'>
-      <div>
-        <img src={coding} alt="coding" />
+      <div className='flex justify-center items-center mt-10'>
+        <img className='w-[25rem]' src={coding} alt="coding" />
       </div>
-        <div className='baslik'>
-            <h1>ABOUT THIS PAGE</h1>
+        <div className=' flex flex-col justify-center items-center text-center mt-10 mb-5'>
+            <h1 className='text-3xl'>ABOUT THIS PAGE</h1>
+            <p className='bg-green-700 w-[17rem] h-[0.3rem] rounded-lg mb-3'></p>
+            <p>NOTE: Some informations are missing because of the API. If you can't see the page properly please go back and try again.</p>
         </div>
-        <div className='tools'>
+        <div className='flex flex-col justify-center items-center gap-2'>
+            <h1 className='text-2xl'>TOOLS</h1>
+            <p className='bg-green-700 w-[15rem] h-[0.3rem] rounded-lg'></p>
             <p>React</p>
             <p>HTML</p>
             <p>TAILWIND CSS</p>
@@ -19,11 +23,12 @@ const About = () => {
             <p>REACT ROUTER DOM v6</p>
             <p>REACT ICONS</p>
         </div>
-        <div className='mylinks'>
-            <h1>Social Media Links</h1>
-            <a href="https://www.linkedin.com/in/menfavakitsayan/">Linkedin</a>
-            <a href="https://github.com/qxan">Github</a>
-            <a href="https://github.com/qxan/react-reacipe-app">Source Code</a>
+        <div className='flex flex-col justify-center items-center mt-5 mb-10 gap-3'>
+            <h1 className='text-2xl'>Usefull Links</h1>
+            <p className='bg-green-700 w-[15rem] h-[0.3rem] rounded-lg'></p>
+            <a href="https://www.linkedin.com/in/menfavakitsayan/" className="hover:underline hover:text-red-800">Linkedin</a>
+            <a href="https://github.com/qxan" className="hover:underline hover:text-red-800">Github</a>
+            <a href="https://github.com/qxan/react-reacipe-app" className="hover:underline hover:text-red-800">Source Code</a>
         </div>
     </div>
   )
